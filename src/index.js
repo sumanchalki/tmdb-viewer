98cf7871a07b2663857684eaed3f54a2e757fff9
sumanchalki <suman.abc@gmail.com>
2019-04-29 22:50:03 +0530

    Updated react-scripts package.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom';
@@ -45,4 +44,3 @@ ReactDOM.render((
     </Router>
   </Provider>
 ), document.getElementById('root'));
-registerServiceWorker();
